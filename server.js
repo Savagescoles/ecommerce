7ec6408e8246623572db8e9413747fee4e8f2da1
Ben Savage <bensavage100@gmail.com>
2022-05-03 18:10:34 -0700

    outline and server.js framework

---
+++ server.js
@@ -0,0 +1,23 @@
+// Dependants //
+const express = require('express');
+const routes = require('./routes');
+const sequelize = require('./config/connection');
+ 
+// Express //
+const app = express();
+const PORT = process.env.PORT || 3002;
+
+// Sets up the Express app to handle data parsing //
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Routes //
+
+app.use(routes);
+
+// sequelize the db and turn it all on //
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => {
+      console.log(`App listening on port ${PORT}!`);
+    });
+});
